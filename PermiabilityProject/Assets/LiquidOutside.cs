25c1fda1830ced3abb4c3dc1ccb76aece5428084
zeromike12 <47909403+zeromike12@users.noreply.github.com>
2026-04-18 16:26:04 -0400

    Liquid Logic

--- PermiabilityProject/Assets/LiquidOutside.cs
+++ PermiabilityProject/Assets/LiquidOutside.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 public class LiquidOutside : MonoBehaviour
 {
     Renderer renderer;// dont touch
-    public float liquidSize=1;// size of the chemical. Smaller wins over larger.
+    Renderer rendererOther;// dont touch
+    public float liquidSize= 1;// size of the chemical. Smaller wins over larger.
     public Color liquidColor = Color.blue;// base color of chemical do not change.
+    public GameObject insideBag;
+    public float insideliquidSize = 1;// size of the chemical. Smaller wins over larger.
+    public Color insideliquidColor = Color.blue;// base color of chemical do not change.
     public bool runSim = false; // check to run simulation. This can be changed outside of the script with a start or stop.
+    Material mat;
+    Material matOther;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Beaker
         renderer = GetComponent<Renderer>();
-        Material mat = renderer.material;//material controls for color
+        mat = renderer.material;//material controls for color
         mat.SetColor("_BaseColor", liquidColor);// how to change color
+        //Inside Bag
+        rendererOther = insideBag.GetComponent<Renderer>();
+        matOther = rendererOther.material;//material controls for color
+        matOther.SetColor("_BaseColor", insideliquidColor);// how to change color
     }
 
     // Update is called once per frame
@@ -19,13 +30,15 @@ public class LiquidOutside : MonoBehaviour
     {
         if (runSim)//runs the simulation
         {
-            if(liquidSize<0)//todo: set 0 to the variable of the inside bag size, setup similar script to this for the inside bag.
+            if(liquidSize<insideliquidSize)//todo: set 0 to the variable of the inside bag size, setup similar script to this for the inside bag.
             {
-                //todo: change bag color and increase size
+                mat.SetColor("_BaseColor", liquidColor);
+                matOther.SetColor("_BaseColor", liquidColor);// how to change color
             }
-            else if (liquidSize>0)//same as before replace with variable of inside bag size
+            else if (liquidSize>insideliquidSize)//same as before replace with variable of inside bag size
             {
-                //todo: change the beaker color and decrease bag size
+                mat.SetColor("_BaseColor", insideliquidColor);
+                matOther.SetColor("_BaseColor", insideliquidColor);// how to change color
             }
             else
             {
